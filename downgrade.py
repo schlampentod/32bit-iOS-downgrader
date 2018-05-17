f1a55bda59641db0b5e762039f7fc30e5a2297b0
Lunarixus <35746622+Lunarixus@users.noreply.github.com>
2018-05-17 22:37:23 +0100

    Add protection against flash without baseband

--- downgrade.py
+++ downgrade.py
@@ -1,4 +1,5 @@
 #import modules that make this program work
+import os
 import subprocess
 print (" _    ___  __      ___                                               _             ")
 print ("(_)  /___\/ _\    /   \ ___ __      __ _ __    __ _  _ __  __ _   __| |  ___  _ __ ")
@@ -6,18 +7,29 @@ print ("| | //  //\ \    / /\ // _ \\ \ /\ / /| '_ \  / _` || '__|/ _` | / _` | 
 print ("| |/ \_// _\ \  / /_//| (_) |\ V  V / | | | || (_| || |  | (_| || (_| ||  __/| |   ")
 print ("|_|\___/  \__/ /___,'  \___/  \_/\_/  |_| |_| \__, ||_|   \__,_| \__,_| \___||_|   ")
 print ("                                              |___/                                ")
-print ("This program only works on the following 32bit iOS devices")
-print ("iPhone 4, 4S, 5, 5C, iPod Touch 4G, 5G, iPad 2, 3, 4, Mini 1st Gen")
-print ("The device needs to be in kDFU")
+print ("[Info] This program only works on the following 32bit iOS devices")
+print ("[Info] iPhone 4, 4S, 5, 5C, iPod Touch 4G, 5G, iPad 2, 3, 4, Mini 1st Gen")
+print ("[Info] The device needs to be in kDFU")
+
+#ask user for the path to the futurerestore executable (helps with multi platform support)
+executable_path = input("[User input] Path to futurerestore executable (eg. futurerestore_macos): ")
 
 #ask the user for their blob and ipsw
-blob = input("Path to SHSH blob: ")
-ipsw = input("Path to ipsw: ")
-bb = input("Flash baseband? (Select n for Wi-Fi-only devices)[y/n]:")
+blob = input("[User input] Path to SHSH blob: ")
+ipsw = input("[User input] Path to ipsw: ")
+
+#start a loop to stop user from accidentally flashing without baseband
+while True:
+	bb = input("[User input] Flash baseband? (Select n for Wi-Fi-only devices)[y/n]:")
+	if bb != "y" or "n":
+		print("Please enter a valid option!")
+		continue
 
 
 #start the restore
+print("[Info] Starting the restore... If your device doesn't do anything then you probably typed something in wrong.")
 if bb == 'y':	
-	subprocess.call(['./futurerestore_macos', '--latest-baseband', '--use-pwndfu', '-t', blob, ipsw])
+	subprocess.call([executable_path, '--latest-baseband', '--use-pwndfu', '-t', blob, ipsw])
 else:
-	subprocess.call(['./futurerestore_macos', '--no-baseband', '--use-pwndfu', '-t', blob, ipsw])
+	subprocess.call([executable_path, '--no-baseband', '--use-pwndfu', '-t', blob, ipsw])
+	
